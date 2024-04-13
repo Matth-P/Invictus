38349fba70ecc0297f81db4613e949e388ae6976
Matth-P <109482342+Matth-P@users.noreply.github.com>
2024-04-12 21:32:23 -0300

    Criação da calculadora base

---
+++ Calculadora/Calc.py
@@ -0,0 +1,11 @@
+class Calculator:
+    def sum(self, a,b):
+        return a + b
+    def sub(self, a,b):
+        return a - b
+    def mult(self, a,b):
+        return a * b
+    def div(self, a,b):
+        if b == 0:
+            return ZeroDivisionError("Erro matemático: Divisão por 0")
+        return a/b
